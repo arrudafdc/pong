be91c81836bde06be26638947cf465758ae6cf27
Lucas Arruda <arrudafdc@gmail.com>
2022-05-02 22:37:58 -0300

    Start animation

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
+ctx.fillStyle = "#FFF";
+ctx.font = "16px Monospace";
+
 let keys = {};
 
 const ball = {
@@ -32,10 +35,31 @@ const rightBar = {
 };
 
 function draw() {
-  ctx.fillStyle = "#FFF";
+  ctx.clearRect(0, 0, canvas.height, canvas.width);
+
   ctx.fillRect(leftBar.x, leftBar.y, leftBar.width, leftBar.height);
   ctx.fillRect(rightBar.x, rightBar.y, rightBar.width, rightBar.height);
   ctx.fillRect(ball.x, ball.y, ball.width, ball.height);
+
+  ctx.fillText(`Player 1: ${leftBar.score}`, 10, 20);
+  ctx.fillText(`Player 2: ${rightBar.score}`, 493, 20);
 }
 
-draw();
+setInterval(draw, 10);
+
+function handleKeydown(event) {
+  if (event.key === "w" && leftBar.y > 0) {
+    leftBar.y = leftBar.y - leftBar.speed;
+  }
+  if (event.key === "s" && leftBar.y < 480) {
+    leftBar.y = leftBar.y + leftBar.speed;
+  }
+  if (event.key === "ArrowUp" && rightBar.y > 0) {
+    rightBar.y = rightBar.y - rightBar.speed;
+  }
+  if (event.key === "ArrowDown" && rightBar.y < 480) {
+    rightBar.y = rightBar.y + rightBar.speed;
+  }
+}
+
+window.addEventListener("keydown", handleKeydown);
